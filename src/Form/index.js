1a866620f610cf374af5344796df92679fb35816
Justyna Boesche <justyna.boesche@gmail.com>
2023-02-03 00:16:20 +0100

    focus to input by using useRef added

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,29 +1,39 @@
 import "./style.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Form = ({ addNewTask }) => {
     const [newTaskContent, setNewTaskContent] = useState("");
+    const inputRef = useRef(null);
 
     const onFormSubmit = (event) => {
         event.preventDefault();
         if (newTaskContent.trim() === "") {
             return;
         }
-        
+
         addNewTask(newTaskContent.trim());
         setNewTaskContent("")
+    };
+
+    const focusInput = () => {
+        inputRef.current.focus();
     };
 
     return (
         <form className="form" onSubmit={onFormSubmit}>
             <input
+                ref={inputRef}
                 value={newTaskContent}
                 className="form__field"
                 placeholder="Co jest do zrobienia?"
                 autoFocus
                 onChange={({ target }) => setNewTaskContent(target.value)}
             />
-            <button className="form__button">Dodaj zadanie</button>
+            <button
+                onClick={focusInput} className="form__button"
+            >
+                Dodaj zadanie
+            </button>
         </form>
     )
 };
