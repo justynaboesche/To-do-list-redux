4da849c6dd1ab6767e195e19995269084cbed749
Justyna Boesche <justyna.boesche@gmail.com>
2023-02-13 13:41:36 +0100

    GlobalStyles created and implemented

---
+++ src/GlobalStyles.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+    
+  *, ::after, ::before {
+    box-sizing:inherit;
+  }
+  body {
+    font-family: 'Montserrat', sans-serif;
+    margin:auto;
+    padding: 30px;
+    background-color: ${({ theme }) => theme.color.gallery};
+    word-break: break-word;
+    overflow-wrap: break-word;
+  }
+`;
--- src/index.js
+++ src/index.js
@@ -5,14 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { theme } from "./theme";
 import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from './GlobalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <App />
     </ThemeProvider>
-
   </React.StrictMode>
 );
 
