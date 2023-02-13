0137d6df434e5636eaed699af11446f8bb17802c
Justyna Boesche <justyna.boesche@gmail.com>
2023-02-13 13:16:05 +0100

    styled components Wrapper, Button to Buttons added

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,26 +1,25 @@
-import "./style.css";
+import { Wrapper, Button } from "./styled";
 
 const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
-    <div className="section__header--buttons">
+    <Wrapper>
         {tasks.length > 0 && (
             <>
-                <button
+                <Button
                     onClick={toggleHideDone} 
                     className="section__header--button"
                 >
                     {hideDone ? "Pokaż" : "Ukryj"} ukończone
-                </button>
-                <button
+                </Button>
+                <Button
                     onClick={setAllDone}
                     className="section__header--button"
                     disabled={tasks.every(({ done }) => done)}
                 >
                     Ukończ wszystkie
-                </button>
+                </Button>
             </>
         )}
-
-    </div>
+    </Wrapper>
 
 );
 
---
+++ src/Buttons/styled.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+
+    @media (max-width: 767px) {
+        display: flex;
+        flex-direction: column;
+        align-items: center;  
+ }
+`;
+
+export const Button = styled.button`
+    border: none;
+    color: ${({ theme }) => theme.color.teal};
+    background-color: transparent;
+    cursor: pointer;
+    padding: 20px; 
+    transition: color 1s;
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        padding: 10px;
+    }
+
+    &:hover {
+        color: brightness(110%);
+    }
+        
+    &:active {
+        color: brightness(120%);
+    }
+
+    &:disabled {
+        color: ${({ theme }) => theme.color.silver};
+        cursor: not-allowed;
+    }
+ `;
