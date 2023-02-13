9c1bb99b5c2b348b6f7e7310c271218e259c333d
Justyna Boesche <justyna.boesche@gmail.com>
2023-02-13 12:43:34 +0100

    a file theme.js created

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+    color: {
+        black: "#000",
+        silver: "#ccc",
+        alto: "#ddd",
+        gallery: "#eee",
+        white: "#fff",
+        teal: "hsl(180, 100%, 25%)",
+        crimson: "#db143c",
+        forestGreen: "#228c22",
+    },
+    breakpoint: {
+        mobileMax: 767,
+    },
+};
