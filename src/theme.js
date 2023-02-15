d2cd00e4e6c8d08ef80992c0d3cb6f47f8e3ef42
Justyna Boesche <justyna.boesche@gmail.com>
2023-02-15 09:27:00 +0100

    change iof the format of the teal color in theme

--- src/theme.js
+++ src/theme.js
@@ -5,7 +5,7 @@ export const theme = {
         alto: "#ddd",
         gallery: "#eee",
         white: "#fff",
-        teal: "hsl(180, 100%, 25%)",
+        teal: "#008080",
         crimson: "#db143c",
         forestGreen: "#228c22",
     },
