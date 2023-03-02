024e63edb2f7a037840fadc9d77682ca0f802a34
Justyna Boesche <justyna.boesche@gmail.com>
2023-03-02 13:53:32 +0100

    application split into features

---
+++ src/features/tasks/Tasks.js
@@ -0,0 +1,51 @@
+import Buttons from "./Buttons";
+import Form from "./Form";
+import TaskList from "./TaskList";
+import Section from "../../common/Section";
+import Header from "../../common/Header";
+import {Container} from "../../common/Container/styled";
+import { useTasks } from '../../useTasks';
+
+function Tasks() {
+  const {
+    hideDone,
+    tasks,
+    toggleHideDone,
+    removeTask,
+    toggleTaskDone,
+    setAllDone,
+    addNewTask,
+  } = useTasks();
+
+  return (
+    <Container>
+      <Header title="Lista zadań" />
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form addNewTask={addNewTask} />}
+      />
+
+      <Section
+        title="Lista zadań"
+        body={
+          <TaskList
+            tasks={tasks}
+            hideDone={hideDone}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
+          />
+        }
+        extraHeaderContent={
+          <Buttons
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
+          />
+        }
+      />
+    </Container>
+  );
+}
+
+export default Tasks;
