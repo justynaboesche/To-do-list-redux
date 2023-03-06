ac606464920a1f793a825793b3ee419a1a1142fa
Justyna Boesche <justyna.boesche@gmail.com>
2023-03-06 17:36:12 +0100

    addNewTask, toggleHideDone, toggleTaskDone from useTasks custom hook removed

--- src/useTasks.js
+++ src/useTasks.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 
 export const useTasks = () => {
-  const [hideDone, setHideDone] = useState(false);
 
   const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || []);
 
@@ -9,22 +8,8 @@ export const useTasks = () => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
-  const toggleHideDone = () => {
-    setHideDone(hideDone => !hideDone);
-  };
-
   const removeTask = (id) => {
     setTasks(tasks => tasks.filter(task => task.id !== id));
-  };
-
-  const toggleTaskDone = (id) => {
-    setTasks(tasks => tasks.map(task => {
-      if (task.id === id) {
-        return { ...task, done: !task.done };
-      }
-
-      return task;
-    }));
   };
 
   const setAllDone = () => {
@@ -34,24 +19,8 @@ export const useTasks = () => {
     })));
   };
 
-  const addNewTask = (content) => {
-    setTasks(tasks => [
-      ...tasks,
-      {
-        content,
-        done: false,
-        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
-      },
-    ])
-  }
-
   return {
-    hideDone,
-    tasks,
-    toggleHideDone,
     removeTask,
-    toggleTaskDone,
     setAllDone,
-    addNewTask,
   };
 };
