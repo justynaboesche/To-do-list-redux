d2dc1d5832086957dd40644e98e0e8de5efb800c
Justyna Boesche <justyna.boesche@gmail.com>
2023-03-06 17:29:14 +0100

    store and provider in app implemented

--- src/index.js
+++ src/index.js
@@ -4,48 +4,52 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { theme } from "./theme";
 import { ThemeProvider } from "styled-components";
+import { Provider } from 'react-redux';
 import { GlobalStyle } from './GlobalStyle';
-import { configureStore } from '@reduxjs/toolkit';
+import store from './store';
+// import { configureStore } from '@reduxjs/toolkit';
 
-const initialState = {
-  tasks: [],
-};
+// const initialState = {
+//   tasks: [],
+// };
 
-const tasksReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "addTask":
-      return {
-        ...state,
-        tasks: [...state.tasks, { content: action.payload }],
-      };
-    default:
-      return state;
-  }
-};
+// const tasksReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case "addTask":
+//       return {
+//         ...state,
+//         tasks: [...state.tasks, { content: action.payload }],
+//       };
+//     default:
+//       return state;
+//   }
+// };
 
-const addTask = content => ({
-  type: "addTask",
-  payload: content,
- });
+// const addTask = content => ({
+//   type: "addTask",
+//   payload: content,
+//  });
 
- const selectTasks = state => state.tasks;
- 
-const store = configureStore({ reducer: tasksReducer });
-console.log(selectTasks(store.getState()));
+//  const selectTasks = state => state.tasks;
 
-store.dispatch(addTask("zad 1"));
-console.log(selectTasks(store.getState()));
+// const store = configureStore({ reducer: tasksReducer });
+// console.log(selectTasks(store.getState()));
 
-store.dispatch(addTask("zad 2"));
-console.log(selectTasks(store.getState()));
+// store.dispatch(addTask("zad 1"));
+// console.log(selectTasks(store.getState()));
+
+// store.dispatch(addTask("zad 2"));
+// console.log(selectTasks(store.getState()));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
