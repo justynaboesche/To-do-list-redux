06cf7452dd3aa599b24b8c4a9960184e3b2f812c
Justyna Boesche <justyna.boesche@gmail.com>
2023-03-06 17:34:15 +0100

    tasksSlice with reducers:  addTask, toggleHideDone, toggleTaskDone, removeTask, removeTask created

---
+++ src/features/tasks/tasksSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const tasksSlice = createSlice({
+    name: "tasks",
+    initialState: {
+        tasks: [],
+        hideDone: false,
+    },
+    reducers: {
+        addTask: ({ tasks }, { payload: task }) => {
+            tasks.push(task);
+        },
+        toggleHideDone: state => {
+            state.hideDone = !state.hideDone;
+        },
+        toggleTaskDone: ({ tasks }, { payload: task }) => {
+            const index = tasks.findIndex(({ id }) => id === task);
+            tasks[index].done = !tasks[index].done;
+        },
+        removeTask: ({ tasks }, { payload: taskId }) => {
+            const index = tasks.findIndex(({ id }) => id === taskId);
+            tasks.splice(index, 1);
+        },
+        setAllDone: ({ tasks }) => {
+            for (const task of tasks) {
+                task.done = true;
+            }
+        },
+    }});
+
+export const { addTask, toggleHideDone, toggleTaskDone, removeTask, setAllDone } = tasksSlice.actions;
+export const selectTasks = state => state.tasks;
+export default tasksSlice.reducer;
