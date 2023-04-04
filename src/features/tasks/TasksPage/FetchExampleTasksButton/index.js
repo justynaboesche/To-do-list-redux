aae56f3d3d65ca5e016d65140ea3dd33fb64889d
Justyna Boesche <justyna.boesche@gmail.com>
2023-04-04 13:53:42 +0200

    loading to FetchExampleTasksButton added

--- src/features/tasks/TasksPage/FetchExampleTasksButton/index.js
+++ src/features/tasks/TasksPage/FetchExampleTasksButton/index.js
@@ -1,18 +1,20 @@
 import { useSelector, useDispatch } from "react-redux";
 import { ButtonsWrapper } from "../Buttons/styled";
 import { Button } from "../Button/styled";
-import {
-    fetchExampleTasks,
-} from "../../tasksSlice";
+import { fetchExampleTasks, selectIsLoading } from "../../tasksSlice";
 
 const FetchExampleTasksButton = () => {
 
     const dispatch = useDispatch();
+    const loading = useSelector(selectIsLoading);
 
     return (
         <ButtonsWrapper>
-            <Button onClick={() => dispatch(fetchExampleTasks())}>
-                Pobierz przykładowe zadania
+            <Button
+                onClick={() => dispatch(fetchExampleTasks())}
+                disabled={loading}
+            >
+                {loading ? "Trwa ładowanie" : "Pobierz przykładowe zadania"}
             </Button>
         </ButtonsWrapper>
     );
