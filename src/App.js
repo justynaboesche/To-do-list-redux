4cd4724a728ef425b94bd85a344c8aaeac145ddf
Justyna Boesche <justyna.boesche@gmail.com>
2023-04-04 16:39:05 +0200

    react-router framework to app added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,27 @@
-import Tasks from "./features/tasks/Tasks";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import TasksPage from "./features/tasks/TasksPage";
+import TaskPage from "./features/tasks/TaskPage";
+import AuthorPage from "./features/author/AuthorPage";
+import Navigation from "./common/Navigation";
+import { toAuthor, toTask, toTasks } from "./routes";
 
-export default () => <Tasks />;
+export default () => (
+  <HashRouter>
+    <Navigation />
+    <Switch>
+      <Route path={toTask()}>
+        <TaskPage />
+      </Route>
+      <Route path={toTasks()}>
+        <TasksPage />
+      </Route>
+      <Route path={toAuthor()}>
+        <AuthorPage />
+      </Route>
+      <Route>
+        <Redirect to={toTasks()} />
+      </Route>
+    </Switch>
+  </HashRouter>
+);
+
