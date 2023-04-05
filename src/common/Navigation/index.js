dca86f38ef0e8b90c8b1f314315db529f6d37b04
Justyna Boesche <justyna.boesche@gmail.com>
2023-04-05 10:24:36 +0200

    return from Navigation removed

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -1,23 +1,21 @@
 import { toAuthor, toTasks } from "../../routes"
 import { Nav, List, Item, StyledNavLink } from "./styled";
 
-const Navigation = () => {
-  return (
-    <Nav>
-      <List>
-        <Item>
-          <StyledNavLink to={toTasks()}>
-            Zadania
-          </StyledNavLink>
-        </Item>
-        <Item>
-          <StyledNavLink to={toAuthor()}>
-            O autorze
-          </StyledNavLink>
-        </Item>
-      </List>
-    </Nav>
-  );
-};
+const Navigation = () => (
+  <Nav>
+    <List>
+      <Item>
+        <StyledNavLink to={toTasks()}>
+          Zadania
+        </StyledNavLink>
+      </Item>
+      <Item>
+        <StyledNavLink to={toAuthor()}>
+          O autorze
+        </StyledNavLink>
+      </Item>
+    </List>
+  </Nav>
+);
 
 export default Navigation;
